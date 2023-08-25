ceefcc85aaaa6595eddf5cee45d6107f12a8d1b0
nitjanan <nitjan.rs@gmail.com>
2023-08-25 16:21:08 +0700

    โม่ 3 เวอร์ชั่น

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -544,13 +544,13 @@ namespace SerialPortListener
             try
             {
                 //แสดงเลขน้ำหนักที่กำลังวิ่ง
-                /* เครื่องพี่จ๋า */
-                
-                string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("KG"));
-                string remainingText = newString.Substring(newString.LastIndexOf("\r"));
+
+
+                string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("\r"));
+                string remainingText = newString.Substring(newString.LastIndexOf("p"));
                 MatchCollection mc = Regex.Matches(remainingText, @"\d+");
-                
-                /* เครื่องพี่รุ่ง */
+
+
                 //MatchCollection mc = Regex.Matches(str, @"\d+");
 
                 if (mc.Count > 0)
@@ -566,7 +566,8 @@ namespace SerialPortListener
                     }
                 }
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
 
             }
 
@@ -1919,6 +1920,7 @@ namespace SerialPortListener
         {
             if (tbCustomerId.Text == "09-A-001" || tbCustomerId.Text == "09-V-001")
             {
+                /*
                 using (var form = new FCancelPassword())
                 {
                     var result = form.ShowDialog();
@@ -1927,6 +1929,7 @@ namespace SerialPortListener
                         string password = form.ReturnPassword;
                         if (password == "pdg]bd=yj'")
                         {
+                */
                             tbWeightIn.Text = "0.00";
                             tbWeightOut.Text = "0.00";
                             tbWeightTotal.Text = "0.00";
@@ -1936,6 +1939,7 @@ namespace SerialPortListener
                             tbVat.Text = "0.00";
                             tbQ.Text = "0.00";
                             return true;
+                /*
                         }
                         else
                         {
@@ -1946,6 +1950,7 @@ namespace SerialPortListener
                         return false;
                     }
                 }
+                */
             }
             return true;
         }
