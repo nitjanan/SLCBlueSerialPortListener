f59ef5b5e2e0170a795b99627fb83a3ab3673dcb
nitjanan <nitjan.rs@gmail.com>
2023-10-26 10:06:19 +0700

    refresh

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -653,19 +653,19 @@ namespace SerialPortListener
             {
                 //แสดงเลขน้ำหนักที่กำลังวิ่ง
                 /* เครื่องพี่จ๋า */
-                //string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("KG"));
-                //string remainingText = newString.Substring(newString.LastIndexOf("\r"));
-               //MatchCollection mc = Regex.Matches(remainingText, @"\d+");
+               string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("KG"));
+               string remainingText = newString.Substring(newString.LastIndexOf("\r"));
+               MatchCollection mc = Regex.Matches(remainingText, @"\d+");
 
                 /* เครื่องพี่รุ่ง */
-                MatchCollection mc = Regex.Matches(str, @"\d+");
+                //MatchCollection mc = Regex.Matches(str, @"\d+");
 
                 if (mc.Count > 0)
                 {
                     if (String.Compare(tbWeigtData.Text, mc[0].Value) != 0)
                     {
                         tbWeigtData.Text = mc[0].Value.TrimStart('0').PadLeft(1, '0');
-                        tbWeigtData.ForeColor = Color.LightCoral;
+                        //tbWeigtData.ForeColor = Color.LightCoral;
                     }
                     else
                     {
