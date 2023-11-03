bb328a6a1cf1dad60746bc240307a5dd615b7e3f
nitjanan <nitjan.rs@gmail.com>
2023-11-03 20:07:51 +0700

    fix cal q

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -1672,14 +1672,25 @@ namespace SerialPortListener
             }
         }
 
-        private void calculatenumQ() {
-            try {
-                double numCalQ = Convert.ToDouble(strCalQ);
-                double numWeightTotal = Convert.ToDouble(tbWeightTotal.Text);
-                double numQ = numWeightTotal / (numCalQ * 1000);
-                tbQ.Text = numQ.ToString("#,##0.00");
+        private void calculatenumQ()
+        {
+            try
+            {
+                if (!checkZeroStr(tbWeightIn.Text) && !checkZeroStr(tbWeightOut.Text))
+                {
+                    double numCalQ = Convert.ToDouble(strCalQ);
+                    double numWeightTotal = Convert.ToDouble(tbWeightTotal.Text);
+                    double numQ = numWeightTotal / (numCalQ * 1000);
+                    tbQ.Text = numQ.ToString("#,##0.00");
+                }
+                else
+                {
+                    tbQ.Text = "0.00";
+                }
+
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
 
             }
         }
@@ -2488,7 +2499,6 @@ namespace SerialPortListener
         private void showErrorWeightInEmty() {
             showErrorEmtyRadioButton(groupBox2);
             showErrorEmtyComboBox(cbbStoneType);
-            showErrorEmtyComboBox(cbbStoneColor);
             showErrorEmtyComboBox(cbbTransport);
             showErrorEmtyTextBox(tbCarLicense);
             showErrorEmtyTextBox(tbCarCity);
@@ -2498,7 +2508,6 @@ namespace SerialPortListener
         private void showErrorWeightOutEmty()
         {
             showErrorEmtyRadioButton(groupBox2);
-            showErrorEmtyComboBox(cbbStoneColor);
             showErrorEmtyTextBox(tbScoopId);
             showErrorEmtyTextBox(tbScoopName);
             //showErrorEmtyRadioButton(groupBox1);
@@ -2653,11 +2662,23 @@ namespace SerialPortListener
         private void tbWeightIn_Leave(object sender, EventArgs e)
         {
             convertFormatToDecimal(tbWeightIn);
+            checkNumWeightMany(tbWeightIn);
         }
 
         private void tbWeightOut_Leave(object sender, EventArgs e)
         {
             convertFormatToDecimal(tbWeightOut);
+            checkNumWeightMany(tbWeightOut);
+        }
+
+        private void checkNumWeightMany(TextBox tb)
+        {
+            if (tb.Text.Length > 9)
+            {
+                MessageBox.Show("ช่อง " + tb.AccessibleName + "มีน้ำหนักเกิน กรุณากรอกข้อมูลใหม่", "แจ้งเตือน", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tb.Text = "0.00";
+            }
+
         }
 
         private void tbCarLicense_KeyUp(object sender, KeyEventArgs e)
