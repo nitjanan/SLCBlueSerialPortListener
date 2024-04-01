0bbb9b38bb656d43b3e6da993a24c7d5efef2a74
nitjanan <nitjan.rs@gmail.com>
2024-04-01 09:10:21 +0700

    sucess

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -1789,6 +1789,7 @@ namespace SerialPortListener
             Weight.Transport = strNotEmty(cbbTransport.Text);
             Weight.OilContent = zeroNotEmty(tbOilContent.Text);
             Weight.Id = tbId.Text;
+            Company.TLogo = " ";
 
 
             if (mode.Equals(3))
