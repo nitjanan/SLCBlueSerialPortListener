aee5bbe578bc4eb5881d8175c4cb23205dd5950b
nitjanan <nitjan.rs@gmail.com>
2024-05-13 10:56:57 +0700

    fix sucess

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -662,7 +662,7 @@ namespace SerialPortListener
                 /* เครื่องพี่จ๋า */
 
                 string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("\r"));
-                string remainingText = newString.Substring(newString.LastIndexOf("p"));
+                string remainingText = newString.Substring(newString.LastIndexOf("(") + 3);
 
                 MatchCollection mc = Regex.Matches(remainingText, @"\d+");
                 /* เครื่องพี่รุ่ง */
