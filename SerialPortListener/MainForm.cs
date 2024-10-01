4e3e0d68ae3259bb295c26d0c5c4fc7ffbce8be8
nitjanan <nitjan.rs@gmail.com>
2024-10-01 09:48:18 +0700

    แก้ นน.

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -670,20 +670,21 @@ namespace SerialPortListener
             {
                 //แสดงเลขน้ำหนักที่กำลังวิ่ง
                 /* เครื่องพี่จ๋า */
-                /*
-                string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("KG"));
+
+                string newString = tbData.Text.Remove(tbData.Text.LastIndexOf("kg"));
                 string remainingText = newString.Substring(newString.LastIndexOf("\r"));
                 MatchCollection mc = Regex.Matches(remainingText, @"\d+");
-                */
+
+
                 /* เครื่องพี่รุ่ง */
-                MatchCollection mc = Regex.Matches(str, @"\d+");
+                //MatchCollection mc = Regex.Matches(str, @"\d+");
 
                 if (mc.Count > 0)
                 {
                     if (String.Compare(tbWeigtData.Text, mc[0].Value) != 0)
                     {
                         tbWeigtData.Text = mc[0].Value.TrimStart('0').PadLeft(1, '0');
-                        tbWeigtData.ForeColor = Color.LightCoral;
+                        //tbWeigtData.ForeColor = Color.LightCoral;
                     }
                     else
                     {
@@ -691,7 +692,8 @@ namespace SerialPortListener
                     }
                 }
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
 
             }
 
