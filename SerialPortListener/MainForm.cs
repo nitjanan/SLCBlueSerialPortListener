9c23eb593b1100c4ec334f69922d9cf01dae0ad9
Userpc <Userpc@USER>
2026-05-26 11:40:27 +0700

    after update delivery order autosave

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -1209,10 +1209,10 @@ namespace SerialPortListener
 
 
         private async void btSave_Click(object sender, EventArgs e)
-        {   
-           
+        {
+
+            //UpdateDeliveryOrderFromApi before save
             await UpdateDeliveryOrderFromApi();
-            autoSave();
         }
 
         private void autoSave()
@@ -3959,6 +3959,8 @@ namespace SerialPortListener
                 finally
                 {
                     btLoadDO.Enabled = true;
+                    // after update delivery order
+                    autoSave();
                 }
             }
         }
