b81f9f3a6ff96cd0baa843cf4f9529beda2356aa
Userpc <Userpc@USER>
2026-05-26 13:35:25 +0700

    fix do ss

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -1215,7 +1215,8 @@ namespace SerialPortListener
         {
 
             //UpdateDeliveryOrderFromApi before save
-            await UpdateDeliveryOrderFromApi();
+            if(tbDoId.Text != "")
+                await UpdateDeliveryOrderFromApi();
             // after update delivery order
             autoSave();
         }
@@ -3885,8 +3886,6 @@ namespace SerialPortListener
         private async Task UpdateDeliveryOrderFromApi()
         {
 
-            if (tbDoId.Text != "")
-            {
                 string baseUrl = getBaseApi(1);
                 string username = getBaseApi(2);
                 string password = getBaseApi(3);
@@ -4069,7 +4068,6 @@ namespace SerialPortListener
                 {
                     btLoadDO.Enabled = true;
                 }
-            }
         }
 
     }
