7d5143a70f04decfd689f24048f5d3da538f7b95
Userpc <Userpc@USER>
2026-05-27 09:18:51 +0700

    fix is cancel

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -1229,7 +1229,8 @@ namespace SerialPortListener
             return 0;
         }
 
-        private int  getDeliveryNotDoId(string carryType) {
+        private int getDeliveryNotDoId(string carryType)
+        {
 
             int count_id = 0;
 
@@ -1237,7 +1238,7 @@ namespace SerialPortListener
             OdbcCommand pgCommand = (OdbcCommand)dl.sqlConn().CreateCommand();
             StringBuilder sql = new StringBuilder();
             sql.Append("select count(weight_id) as count_id from weight_delivery where ");
-            sql.Append("do_doc_no = '" + tbDoDocNo.Text + "' and carry_type_name = '" + carryType + "' ");
+            sql.Append("do_doc_no = '" + tbDoDocNo.Text + "' and carry_type_name = '" + carryType + "' and is_cancel = false ");
             if (tbId.Text != "")
                 sql.Append(" and weight_id != '" + tbId.Text + "' ");
 
@@ -1540,8 +1541,6 @@ namespace SerialPortListener
 
         private async void prepareWeightDelivery(string tmpDoId, string tmpOldDoId)
         {
-            if (tbDoId.Text != "")
-            {
                 if (tmpOldDoId != tmpDoId)
                 {
 
@@ -1552,8 +1551,6 @@ namespace SerialPortListener
                 {
                     await UCWeightDelivery(tmpDoId, false);
                 }
-
-            }
         }
 
 
