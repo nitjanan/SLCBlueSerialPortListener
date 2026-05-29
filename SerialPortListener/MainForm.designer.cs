2761713701e838d481b7487c7d30fec9b3ffc599
Userpc <Userpc@USER>
2026-05-29 16:33:31 +0700

    fix scoop

--- SerialPortListener/MainForm.designer.cs
+++ SerialPortListener/MainForm.designer.cs
@@ -1768,6 +1768,7 @@
             this.tbDoId.Size = new System.Drawing.Size(57, 23);
             this.tbDoId.TabIndex = 143;
             this.tbDoId.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbDoId.Visible = false;
             // 
             // tbDoDocNo
             // 
@@ -1801,6 +1802,7 @@
             this.tbOldDoId.Size = new System.Drawing.Size(57, 23);
             this.tbOldDoId.TabIndex = 146;
             this.tbOldDoId.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbOldDoId.Visible = false;
             // 
             // cbS
             // 
