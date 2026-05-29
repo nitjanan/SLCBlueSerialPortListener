d2ff63c3bdf1fd6f2fa8c0653b097441d018e4dc
Userpc <Userpc@USER>
2026-05-29 14:42:32 +0700

    delivery order 100%

--- SerialPortListener/MainForm.designer.cs
+++ SerialPortListener/MainForm.designer.cs
@@ -1787,7 +1787,6 @@
             this.tbDoId.Size = new System.Drawing.Size(57, 23);
             this.tbDoId.TabIndex = 143;
             this.tbDoId.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.tbDoId.Visible = false;
             // 
             // tbDoDocNo
             // 
@@ -1821,7 +1820,6 @@
             this.tbOldDoId.Size = new System.Drawing.Size(57, 23);
             this.tbOldDoId.TabIndex = 146;
             this.tbOldDoId.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.tbOldDoId.Visible = false;
             // 
             // MainForm
             // 
