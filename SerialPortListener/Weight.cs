3076429c24f6a06484ff284f8c6443722fd394cf
Userpc <Userpc@USER>
2026-06-15 11:04:27 +0700

    add stone_desc

--- SerialPortListener/Weight.cs
+++ SerialPortListener/Weight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +16,7 @@ namespace SerialPortListener
         private static string _customerId;
         private static string _customerName;
         private static string _stoneType;
+        private static string _stoneDesc;
         private static string _carLicense;
         private static string _carCity;
         private static string _dateIn;
@@ -153,6 +154,14 @@ namespace SerialPortListener
             { return _stoneType; }
             set
             { _stoneType = value; }
+        }
+
+        public static string StoneDesc
+        {
+            get
+            { return _stoneDesc; }
+            set
+            { _stoneDesc = value; }
         }
 
         public static string CarCity
