e463cabbe72c9bcc6870da13bcb50d44cd195e2b
Userpc <Userpc@USER>
2026-07-01 14:12:35 +0700

    เพิ่มการควบคุม com port ผ่าน ucHelp

--- SerialPortListener/MainForm.cs
+++ SerialPortListener/MainForm.cs
@@ -200,7 +200,7 @@ namespace SerialPortListener
 
             getSettingDefault();
 
-            _spManager.StartListening();
+            // _spManager.StartListening();
         }
 
         public void getSettingDefault()
@@ -874,6 +874,7 @@ namespace SerialPortListener
         {
             //Serial Port
             _spManager = new SerialPortManager();
+            ucHelp.SetSerialPortManager(_spManager);
             SerialSettings mySerialSettings = _spManager.CurrentSerialSettings;
             serialSettingsBindingSource.DataSource = mySerialSettings;
             /*
