834ce1936929afb46abfca8a704f307ac077390c
Userpc <Userpc@USER>
2026-07-20 11:10:13 +0700

    Add WeightDeliveryReport

---
+++ SerialPortListener/FPrintWeightDelivery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPortListener
+{
+    public partial class FPrintWeightDelivery : Form
+    {
+        Microsoft.Reporting.WinForms.ReportDataSource _rs;
+
+        public FPrintWeightDelivery(Microsoft.Reporting.WinForms.ReportDataSource rs)
+        {
+            InitializeComponent();
+            _rs = rs;
+        }
+
+        private void FPrintWeightDelivery_Load(object sender, EventArgs e)
+        {
+            Microsoft.Reporting.WinForms.ReportParameter[] p = new Microsoft.Reporting.WinForms.ReportParameter[] {
+                new Microsoft.Reporting.WinForms.ReportParameter("PDateFrom", WeightTempReport.DateFrom),
+                new Microsoft.Reporting.WinForms.ReportParameter("PDateTo", WeightTempReport.DateTo),
+            };
+
+            this.reportViewer1.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
+            this.reportViewer1.ZoomMode = Microsoft.Reporting.WinForms.ZoomMode.Percent;
+            this.reportViewer1.LocalReport.DataSources.Clear();
+            this.reportViewer1.LocalReport.DataSources.Add(_rs);
+            this.reportViewer1.LocalReport.SetParameters(p);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
